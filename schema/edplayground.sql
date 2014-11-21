9f36d130d96596cd986483ab051894d864b45a45
Marvin Manguiat <marvinmanguiat@gmail.com>
2014-11-22 00:37:52 +0800

    schema updated

--- schema/edplayground.sql
+++ schema/edplayground.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 18, 2014 at 05:29 AM
+-- Generation Time: Nov 22, 2014 at 12:35 AM
 -- Server version: 5.5.38-0ubuntu0.14.04.1-log
 -- PHP Version: 5.5.9-1ubuntu4.4
 
@@ -53,24 +53,23 @@ CREATE TABLE IF NOT EXISTS `encodingJobs` (
   `video_id` int(11) NOT NULL,
   `source` text NOT NULL,
   `destination` text NOT NULL,
+  `thumbnail1` text,
   `status` varchar(255) NOT NULL,
   `created` datetime NOT NULL,
   `command` text NOT NULL,
   PRIMARY KEY (`job_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
 
 --
 -- Dumping data for table `encodingJobs`
 --
 
-INSERT INTO `encodingJobs` (`job_id`, `video_id`, `source`, `destination`, `status`, `created`, `command`) VALUES
-(1, 0, '/var/www/videos/2014/10/video_1.mp4', '/var/www/videos/2014/10/video_1encoded.mp4', 'completed', '2014-11-17 00:00:00', 'ffmpeg -i /var/www/videos/2014/10/video_1.mp4 /var/www/videos/2014/10/video_1encoded.flv'),
-(2, 0, '/var/www/videos/2014/10/video_2.mp4', '/var/www/videos/2014/10/video_2encoded.flv', 'completed', '2014-11-17 00:00:00', 'ffmpeg -i /var/www/videos/2014/10/video_2.mp4 /var/www/videos/2014/10/video_2encoded.flv'),
-(18, 25, '/var/www/videos/2014/11/video_546a63bbc88d2.mp4', '/var/www/videos/2014/11/video_546a63bbc88d2.mp4.mp4', 'completed', '2014-11-18 05:08:11', ''),
-(19, 26, '/var/www/videos/2014/11/video_546a641666891.mp4', '/var/www/videos/2014/11/video_546a641666891.mp4.mp4', 'completed', '2014-11-18 05:09:42', ''),
-(20, 27, '/var/www/videos/2014/11/video_546a65b17e18f.mp4', '/var/www/videos/2014/11/video_546a65b17e18f.mp4.mp4', 'completed', '2014-11-18 05:16:33', ''),
-(21, 28, '/var/www/videos/2014/11/video_546a6651846da.mp4', '/var/www/videos/2014/11/video_546a6651846da.mp4.mp4', 'completed', '2014-11-18 05:19:13', ''),
-(22, 29, '/var/www/videos/2014/11/video_546a671abc249.mp4', '/var/www/videos/2014/11/video_546a671abc249.mp4.mp4', 'completed', '2014-11-18 05:22:34', '');
+INSERT INTO `encodingJobs` (`job_id`, `video_id`, `source`, `destination`, `thumbnail1`, `status`, `created`, `command`) VALUES
+(1, 0, '/var/www/videos/2014/10/video_1.mp4', '/var/www/videos/2014/10/video_1encoded.mp4', NULL, 'completed', '2014-11-17 00:00:00', 'ffmpeg -i /var/www/videos/2014/10/video_1.mp4 /var/www/videos/2014/10/video_1encoded.flv'),
+(2, 0, '/var/www/videos/2014/10/video_2.mp4', '/var/www/videos/2014/10/video_2encoded.flv', NULL, 'completed', '2014-11-17 00:00:00', 'ffmpeg -i /var/www/videos/2014/10/video_2.mp4 /var/www/videos/2014/10/video_2encoded.flv'),
+(21, 0, '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f5680ba8c9.mp4', '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f5680ba8c9.mp4.mp4', '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f5680ba8c9.mp4.png', 'completed', '2014-11-21 23:13:04', ''),
+(22, 0, '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f63503fbd8.mp4', '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f63503fbd8.mp4.mp4', '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f63503fbd8.mp4.png', 'completed', '2014-11-22 00:07:44', ''),
+(23, 0, '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f662e3ebd3.mp4', '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f662e3ebd3.mp4.mp4', '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f662e3ebd3.mp4.png', 'completed', '2014-11-22 00:19:58', '');
 
 -- --------------------------------------------------------
 
@@ -142,16 +141,18 @@ CREATE TABLE IF NOT EXISTS `video` (
   `dislikes` int(11) DEFAULT '0',
   `publish` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
 
 --
 -- Dumping data for table `video`
 --
 
 INSERT INTO `video` (`id`, `title`, `desc`, `category_id`, `runtime`, `embed_code`, `video_src`, `video_path`, `thumbnail`, `uploaded`, `modified`, `views`, `likes`, `dislikes`, `publish`) VALUES
-(1, 'Paul Gilbert', 'Paul Gilbert & Freddie Nelson - Let It Be (LIVE)', '3', '20:10', NULL, 'http://173.45.66.115/serv/serv.php?id=1', '/videos/2014/10/video_1.mp4', '/img/thumbnails/vids/2014/10/sample320x200.png', '2014-10-28 10:24:11', '2014-09-23 00:00:00', 1061, 12, 10, 1),
-(2, 'Paul red guitar', 'Paul playing red ibanez', '4', '30:41', NULL, 'http://173.45.66.115/serv/serv.php?id=2', '/videos/2014/10/video_2.mp4', '/img/thumbnails/vids/2014/10/sample320x200.png', '2014-02-02 04:10:11', '2014-10-20 00:00:00', 101, 15, 6, 1),
-(6, 'Joe Satriani - Always With Me, Always With You (Live 2006)', 'Joe Satriani - Always With Me, Always', '3', '12:50', NULL, 'http://173.45.66.115/serv/serv.php?id=1', '/videos/2014/10/video_1.mp4', '/img/thumbnails/vids/2014/10/sample320x200-2.png', '2014-10-24 04:14:17', '2014-10-20 00:00:00', 133, 1, 0, 1);
+(1, 'Paul Gilbert', 'Paul Gilbert & Freddie Nelson - Let It Be (LIVE)', '3', '20:10', NULL, 'http://173.45.66.115/serv/serv.php?id=1', '/videos/2014/10/video_1.mp4', '/img/thumbnails/vids/2014/10/sample320x200.png', '2014-10-28 10:24:11', '2014-09-23 00:00:00', 1061, 12, 10, NULL),
+(2, 'Paul red guitar', 'Paul playing red ibanez', '4', '30:41', NULL, 'http://173.45.66.115/serv/serv.php?id=2', '/videos/2014/10/video_2.mp4', '/img/thumbnails/vids/2014/10/sample320x200.png', '2014-02-02 04:10:11', '2014-10-20 00:00:00', 101, 15, 6, NULL),
+(6, 'Joe Satriani - Always With Me, Always With You (Live 2006)', 'Joe Satriani - Always With Me, Always', '3', '12:50', NULL, 'http://192.168.4.167/serv/serv.php?id=1', '/videos/2014/10/video_1.mp4', '/img/thumbnails/vids/2014/10/sample320x200-2.png', '2014-10-24 04:14:17', '2014-10-20 00:00:00', 133, 1, 0, NULL),
+(26, 'killed by single puch', 'Drunk dude gets owned by 1 killer punch', '1', NULL, NULL, NULL, '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f63503fbd8.mp4', '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f63503fbd8.mp4.png', NULL, NULL, 3, 0, 0, 1),
+(27, 'dsafsdfsd', 'fdsfsdf', '2', NULL, NULL, NULL, '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f662e3ebd3.mp4', '/home/marvin/Documents/vhost/marvin.extreme.com/data/videos/2014/11/video_546f662e3ebd3.mp4.png', NULL, NULL, 3, 1, 0, 1);
 
 -- --------------------------------------------------------
 
